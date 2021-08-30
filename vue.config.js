d88d4748214c78140a16b305ab64faeeb783e9a7
Lukas Curtis <llc6wh@virginia.edu>
2021-08-30 18:55:20 -0400

    prod build

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    outputDir: path.resolve(__dirname, '../api/public'),
+    // devServer: {
+    //     proxy: 'http://2d4e39511daf.ngrok.io'
+    // }
+    // devServer: {     
+    //     port: 3000,     
+    //     proxy: "http://localhost/api"   
+    // }
+}
